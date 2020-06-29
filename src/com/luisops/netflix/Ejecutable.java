7847cbfc3883baa8329138b591643b7d1a14f770
Luis Fernando Guerra <luisjeremias33.3@gmail.com>
2020-06-29 04:01:03 -0500

    version 2

--- src/com/luisops/netflix/Ejecutable.java
+++ src/com/luisops/netflix/Ejecutable.java
@@ -1,5 +1,6 @@
 package com.luisops.netflix;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Ejecutable {
@@ -7,14 +8,15 @@ public class Ejecutable {
 	
 	public static void main(String[] args) {
 		
-		//int miArray[] = new int[5];
 		
-		Pelicula miArrayDePeliculas[] = new Pelicula[5];
-	    miArrayDePeliculas[0] = new Pelicula("Rescatando al soldado Ryan", "Drama", "Steven Spielberg", 169, 1998) ;
-		miArrayDePeliculas[1] = new Pelicula("Aprueba de Fuego", "Drama Romance y Cine Religioso");
-		miArrayDePeliculas[2] = new Pelicula();
-		miArrayDePeliculas[3] = new Pelicula("Reto de Valientes", "Drama y Cine Religioso", "Alex Kendrick", 130, 2011);
-		miArrayDePeliculas[4] = new Pelicula("Desafio a los Gigantes", "Drama");
+		
+		ArrayList<Pelicula> miArrayDePeliculas = new ArrayList<Pelicula>();
+		miArrayDePeliculas.add(new Pelicula("Rescatando al soldado Ryan", "Drama", "Steven Spielberg", 169, 1998));
+		miArrayDePeliculas.add(new Pelicula("Aprueba de Fuego", "Drama Romance y Cine Religioso"));
+		miArrayDePeliculas.add(new Pelicula());
+		miArrayDePeliculas.add(new Pelicula("Reto de Valientes", "Drama y Cine Religioso", "Alex Kendrick", 130, 2011));
+		miArrayDePeliculas.add(new Pelicula("Desafio a los Gigantes", "Drama"));
+		
 		
 		System.out.println("--------------Lista de todas la Peliculas-----------------------\n");
 		
@@ -22,13 +24,12 @@ public class Ejecutable {
 			
 			System.out.println(pelicula);
 		}
-	    
-		Serie  miArrayDeSerie[] =  new Serie[5];
-		 miArrayDeSerie[0] =  new Serie("Masha y el Oso", "Aventura y Comedia", "Oleg Kuzukov", 7, 4);
-		 miArrayDeSerie[1] =  new Serie("Shaun the Sheep", "Comedia y Aventura", "Nick Park",7, 6);	
-		 miArrayDeSerie[2] =  new Serie("La Guardia del Leon", "Animación y Aventura");
-		 miArrayDeSerie[3] =  new Serie("Anne with an E", "Drama", "Nick Caro",69, 3);		 
-		 miArrayDeSerie[4] =  new Serie("Bolivar", "Juana Uribe");
+	    ArrayList<Serie> miArrayDeSerie = new ArrayList<Serie>();
+		miArrayDeSerie.add(new Serie("Masha y el Oso", "Aventura y Comedia", "Oleg Kuzukov", 7, 4));
+		miArrayDeSerie.add(new Serie("Shaun the Sheep", "Comedia y Aventura", "Nick Park",7, 6));
+		miArrayDeSerie.add(new Serie("La Guardia del Leon", "Animación y Aventura"));
+		miArrayDeSerie.add(new Serie("Anne with an E", "Drama", "Nick Caro",69, 3));
+		miArrayDeSerie.add( new Serie("Bolivar", "Juana Uribe"));
 	
 		 System.out.println("--------------Lista de todas las Series---------------------\n");
 		
@@ -36,11 +37,17 @@ public class Ejecutable {
 			System.out.println(serie);
 		}
 		//marcar como visto
+		try {
+			 miArrayDePeliculas.get(3).marcarVisto("1h 10m 15s");
+			 miArrayDePeliculas.get(4).marcarVisto("1h 5m 2s");
+			 miArrayDePeliculas.get(6).marcarVisto("1h 4m 3s");//
+			
+			 miArrayDeSerie.get(1).marcarVisto("2m 05s");
+			 miArrayDeSerie.get(3).marcarVisto("5m 10s");
+		} catch (Exception e) {
+			System.out.println("Error al marcar visto");
+		}
 		
-		miArrayDePeliculas[3].marcarVisto("1h 10m 15s");
-		miArrayDePeliculas[4].marcarVisto("1h 5m 2s");
-		miArrayDeSerie[1].marcarVisto("2m 05s");
-		miArrayDeSerie[3].marcarVisto("5m 10s");
 		 
 		System.out.println("------------------Lista de Peliculas que se visualizaron----------------\n");
 		
@@ -70,33 +77,33 @@ public class Ejecutable {
 	}
 	
 
-	public static String peliculaRecinte(Pelicula mispeliculas[]) {
+	public static String peliculaRecinte(ArrayList<Pelicula> misPeliculas) {
 		int maximo = 0;
 		int indice = 0;//3
 		
-		for (int i = 0; i < mispeliculas.length; i++) {
-			if(mispeliculas[i].getAno()>maximo) {
-				maximo = mispeliculas[i].getAno();
+		for (int i = 0; i < misPeliculas.size(); i++) {
+			if(misPeliculas.get(i).getAno()>maximo) {
+				maximo = misPeliculas.get(i).getAno();
 				indice=i;
 			}
 	
 		}
 		
-		return mispeliculas[indice].toString();
+		return misPeliculas.get(indice).toString();
 	}
 	
-	public static String seriesMasVistas(Serie misSeries[]) {
+	public static String seriesMasVistas(ArrayList<Serie> misSeries) {
 		int maximo=0;
 		int indice=0;
 		
-		for (int i = 0; i < misSeries.length; i++) {
-			if(misSeries[i].getNoTemporada()>maximo) {
-				maximo= misSeries[i].getNoTemporada();
+		for (int i = 0; i < misSeries.size(); i++) {
+			if(misSeries.get(i).getNoTemporada()>maximo) {
+				maximo= misSeries.get(i).getNoTemporada();
 				indice=i;
 			}
 			
 		}
 		
-		return misSeries[indice].toString();
+		return misSeries.get(indice).toString();
 	}
 }
